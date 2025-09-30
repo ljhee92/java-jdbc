50621970c63124c22508bd917eb3bfad2bad697a
ljhee92 <weilainib7@gmail.com>
2025-09-30 13:37:26 +0900

    test: non-repeatable reads

--- study/src/test/java/transaction/stage1/Stage1Test.java
+++ study/src/test/java/transaction/stage1/Stage1Test.java
@@ -34,10 +34,10 @@ import static org.assertj.core.api.Assertions.assertThat;
  *   Read phenomena | Dirty reads | Non-repeatable reads | Phantom reads
  * Isolation level  |             |                      |
  * -----------------|-------------|----------------------|--------------
- * Read Uncommitted |      +      |                      |
- * Read Committed   |      -      |                      |
- * Repeatable Read  |      -      |                      |
- * Serializable     |      -      |                      |
+ * Read Uncommitted |      +      |           +          |
+ * Read Committed   |      -      |           +          |
+ * Repeatable Read  |      -      |           -          |
+ * Serializable     |      -      |           -          |
  */
 class Stage1Test {
 
@@ -111,10 +111,10 @@ class Stage1Test {
      *   Read phenomena | Non-repeatable reads
      * Isolation level  |
      * -----------------|---------------------
-     * Read Uncommitted |
-     * Read Committed   |
-     * Repeatable Read  |
-     * Serializable     |
+     * Read Uncommitted | +
+     * Read Committed   | +
+     * Repeatable Read  | -
+     * Serializable     | -
      */
     @Test
     void noneRepeatable() throws SQLException {
